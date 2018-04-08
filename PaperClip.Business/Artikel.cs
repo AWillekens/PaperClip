7a8289396f7435c809dcc6e74e0a43e5dab01949
AWillekens <38178790+AWillekens@users.noreply.github.com>
2018-04-08 21:04:36 +0200

    Add files via upload

---
+++ PaperClip.Business/Artikel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperClip.Business
+{
+    public class Artikel
+    {
+        public string ArtikelNaam { get; set; }
+        public int Aantal { get; set; }
+        public string Omschrijving { get; set; }
+        public int EenheidsPrijs { get; set; }
+        public int AangerekendePrijs { get; set; }
+        public int BtwPercentage {get; set; }
+        public int TotaalExclBtw { get; set; }
+
+    }
+}

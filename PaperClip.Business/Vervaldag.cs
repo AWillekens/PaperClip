0e876f3089464a9b02604f7550c3a929ba60e531
AWillekens <38178790+AWillekens@users.noreply.github.com>
2018-04-08 21:05:08 +0200

    Add files via upload

---
+++ PaperClip.Business/Vervaldag.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperClip.Business
+{
+    public class Vervaldag
+    {
+        public DateTime EindeMaand(DateTime dateTime)
+        {
+            //var vandaag = new DateTime();
+            return dateTime.Add//new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+        public DateTime DertigDagen(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+        public DateTime DertigDagenEindeMaand(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+        public DateTime ZestigDagen(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+        public DateTime ZestigDagenEindeMaand(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+        public DateTime NegentigDagen(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+        public DateTime NegentigDagenEindeMaand(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+    }
+}

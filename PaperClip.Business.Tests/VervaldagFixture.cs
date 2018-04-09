8ec0c6ced73c1d1d3e55e0393d451f837e824292
AWillekens <38178790+AWillekens@users.noreply.github.com>
2018-04-09 22:49:07 +0200

    Add files via upload

---
+++ PaperClip.Business.Tests/VervaldagFixture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaperClip.Business.Tests
+{
+    [TestClass]
+    public class VervaldagFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

6f5b91d122c16e3a4097c5905fea27e4021a4cec
AWillekens <38178790+AWillekens@users.noreply.github.com>
2018-04-09 22:52:30 +0200

    Add files via upload

---
+++ PaperClip.Business/ArtikelGroep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperClip.Business
+{
+    public class ArtikelGroep
+    {
+        public string Naam { get; set; }
+        public Artikel Artikel { get; set; }
+        public StaffelKorting StaffelKorting { get; set; }
+    }
+}

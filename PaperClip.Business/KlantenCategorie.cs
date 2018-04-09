10bf2ae55e842f3070e00d267517e44e271aefdf
AWillekens <38178790+AWillekens@users.noreply.github.com>
2018-04-09 22:33:48 +0200

    Add files via upload

---
+++ PaperClip.Business/KlantenCategorie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperClip.Business
+{
+    public class KlantenCategorie
+    {
+        public string Naam { get; set; }
+        public int Kortingspercentage { get; set; }
+
+    }
+}

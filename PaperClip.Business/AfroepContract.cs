281a0574255c57b508d5718c846a93e45bf0e032
AWillekens <38178790+AWillekens@users.noreply.github.com>
2018-04-09 22:48:11 +0200

    Add files via upload

---
+++ PaperClip.Business/AfroepContract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperClip.Business
+{
+    public class AfroepContract
+    {
+        public string Naam { get; set; }
+        public int Aantal { get; set; }
+    }
+}

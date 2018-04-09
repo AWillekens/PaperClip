04664077c33181c6b020dff8afedc217ac192cfd
AWillekens <38178790+AWillekens@users.noreply.github.com>
2018-04-09 22:32:15 +0200

    Update Factuur.cs

--- PaperClip.Business/Factuur.cs
+++ PaperClip.Business/Factuur.cs
@@ -8,21 +8,22 @@ namespace PaperClip.Business
 {
     public class Factuur
     {
-       // public int FactuurNummer { get; set; }
+        public int FactuurNummer { get; set; }
         public Klant Klant { get; set; }
-        public DateTime Datum { get; set; }
+        public DateTime FactuurDatum { get; set; }
+        public Vervaldag Vervaldag { get; set; }
+
+        public Factuur(int factuurNummer)
+        {
+            FactuurNummer = factuurNummer;
+        }
     
-
-        public int GeefFactuurNummer(int factuurnummer)
+        public int GeefFactuurNummer(int factuurNummer)
         {
-            return factuurnummer +=1;
+            return factuurNummer +=1;
         }
 
-        public int BerekenVervaldag()
-        {
-            DateTime 
-        }
-
+     
     }
 
 }
